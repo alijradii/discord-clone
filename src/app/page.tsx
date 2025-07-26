9c48581b31fef8b30666d62ffd6f481566e07a7b
alijradii <alijradi2003@gmail.com>
2025-07-26 15:34:21 +0300

    added themes and mode toggle button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
+import { ModeToggle } from "@/components/theme/mode-toggle-button";
 import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="">
-      <h1 className="text-red-500">hello world</h1>
-      
-      
-      <UserButton/> 
+      <h1 className="text-primary">hello world</h1>
+      <ModeToggle />
+
+      <UserButton />
     </div>
   );
 }
