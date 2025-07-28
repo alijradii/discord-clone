b1fe732f47f23c028b98e52816b90eb1cfd0d122
alijradii <alijradi2003@gmail.com>
2025-07-28 17:29:38 +0300

    [setup page] added basic content and functionality for create server modal

--- src/components/modals/create-server.tsx
+++ src/components/modals/create-server.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
 import {
   Dialog,
   DialogContent,
@@ -9,15 +13,41 @@ import {
   DialogHeader,
 } from "../ui/dialog";
 import { Button } from "../ui/button";
-import { useForm } from "react-hook-form";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "../ui/input";
+
+const formSchema = z.object({
+  name: z.string().min(3, {
+    error: "Server name is required",
+  }),
+  imageUrl: z.string().min(3, {
+    error: "Server image is required",
+  }),
+});
 
 export const CreateServerModal = () => {
   const form = useForm({
+    resolver: zodResolver(formSchema),
     defaultValues: {
       name: "",
       imageUrl: "",
     },
   });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
 
   return (
     <div className="p-48">
@@ -31,9 +61,39 @@ export const CreateServerModal = () => {
             Give your server a personality with a custom name and image. You can
             always change them later.
           </DialogDescription>
-          <DialogFooter>
-            <Button type="submit">Confirm</Button>
-          </DialogFooter>
+
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <div className="flex items-center jutsify-center text-center">
+                Image Upload
+              </div>
+
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="uppercase text-xs font-bold text-slate-600 dark:text-slate-400/70">
+                      Server Name
+                    </FormLabel>
+
+                    <FormControl>
+                      <Input
+                        disabled={isLoading}
+                        className=""
+                        placeholder="Enter server name"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+
+              <DialogFooter>
+                <Button type="submit">Confirm</Button>
+              </DialogFooter>
+            </form>
+          </Form>
         </DialogContent>
       </Dialog>
     </div>
