e442b67b25605e6c34fb55cccdb29c370a8608f7
alijradii <alijradi2003@gmail.com>
2025-07-31 11:32:51 +0300

    [main] added render user servers to navigation sidebar

---
+++ src/components/navigation/navigation-item.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { Server } from "@/generated/prisma";
+import Image from "next/image";
+import { ActionTooltip } from "../common/action-tooltip";
+
+import { useParams, useRouter } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+export const NavigationItem = ({ server }: { server: Server }) => {
+  const params = useParams();
+  const router = useRouter();
+  return (
+    <ActionTooltip label={server.name} side="right">
+      <button
+        className="group relative flex items-center"
+        onClick={() => router.push(`/servers/${server.id}`)}
+      >
+        <div
+          className={cn(
+            "bg-primary rounded-r-full w-[4px] transition-all absolute left-0",
+            params?.serverid !== server.id && "group-hover:h-[20px]",
+            params?.serverid === server.id && "h-[36px]"
+          )}
+        ></div>
+
+        <div className="w-[48px] h-[48px] cursor-pointer rounded-[48px] overflow-hidden relative ml-[12px]">
+          <div className="relative w-full h-full overflow-hidden">
+            <Image
+              src={server.imageUrl}
+              alt="server image"
+              fill
+              className="object-cover"
+            />
+          </div>
+        </div>
+      </button>
+    </ActionTooltip>
+  );
+};
--- src/components/navigation/navigation-sidebar.tsx
+++ src/components/navigation/navigation-sidebar.tsx
@@ -2,6 +2,9 @@ import { currentProfile } from "@/lib/data/current-profile";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
 import { NavigationAction } from "./navigation-action";
+import { Separator } from "../ui/separator";
+import { ScrollArea } from "../ui/scroll-area";
+import { NavigationItem } from "./navigation-item";
 
 export const NavigationSidebar = async () => {
   const profile = await currentProfile();
@@ -21,8 +24,15 @@ export const NavigationSidebar = async () => {
   });
 
   return (
-    <div className="flex flex-col space-y-4 py-3 items-center h-full text-primary w-full bg-muted">
+    <div className="flex flex-col space-y-4 py-3 items-center h-full text-primary w-full bg-[#080808]">
       <NavigationAction />
+      <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md max-w-10 mx-auto" />
+
+      <ScrollArea className="flex-1 w-full items-center">
+        {servers.map((server) => {
+          return <NavigationItem key={server.id} server={server} />;
+        })}
+      </ScrollArea>
     </div>
   );
 };
